b1ec932a25e2a758847fad2f957b1a768bb6d1a6
DeSolator2008 <markulyka@mail.ru>
2024-04-19 05:38:38 +1000

    Dz5

--- app.js
+++ app.js
@@ -2,6 +2,18 @@ const http = require('http');
 const mainRouteController = require("../1/controllers/main.js");
 const gameRouteController = require("../1/controllers/game.js");
 const voteRouteController = require("../1/controllers/vote.js");
+const express = require('express');
+const mainRoute = require('./routes/main');
+const gamesRouter = require('./routes/games'); 
+
+const PORT = 3000;
+const app = express();
+
+app.use(mainRoute, gamesRouter); 
+
+app.listen(PORT, () => {
+  console.log(`Server is running at PORT http://localhost:${PORT}`);
+}) 
 
 const server = http.createServer((req, res) => {
   const url = req.url;
