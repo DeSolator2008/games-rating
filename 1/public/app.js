1f402fb0e4b9378a94054496c56303f340c248b1
DeSolator2008 <markulyka@mail.ru>
2024-04-23 14:52:35 +1000

    Dz7

--- 1/public/app.js
+++ 1/public/app.js
@@ -1,18 +1,21 @@
-const http = require('http');
-const mainRouteController = require("../1/controllers/main.js");
-const gameRouteController = require("../1/controllers/game.js");
-const voteRouteController = require("../1/controllers/vote.js");
 const express = require('express');
+const path = require('path');
+const bodyParser = require('body-parser');
 const mainRoute = require('./routes/main');
 const gamesRouter = require('./routes/games'); 
 
 const PORT = 3000;
 const app = express();
 
-app.use(mainRoute, gamesRouter); 
+app.use(
+    bodyParser.json(),
+    express.static(path.join(__dirname, 'public')),
+    mainRoute,
+    gamesRouter
+); 
 
 app.listen(PORT, () => {
-  console.log(`Server is running at PORT http://localhost:${PORT}`);
+    console.log(`Server is running at PORT http://localhost:${PORT}`);
 }) 
 
 const server = http.createServer((req, res) => {
