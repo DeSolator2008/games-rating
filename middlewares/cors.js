c2e2e136f3ab00d58837ce2a5a826cc05c6a6430
DeSolator2008 <markulyka@mail.ru>
2024-04-25 18:28:39 +1000

    Dz8

--- middlewares/cors.js
+++ middlewares/cors.js
@@ -2,15 +2,15 @@ const allowedCors = [
   'https://practicum.yandex.ru',
   'https://students-projects.ru',
   'localhost:3000'
-]; 
+];
 
 function cors(req, res, next) {
-    const { origin } = req.headers;
-    
+    const { origin } = req.headers; 
     if (allowedCors.includes(origin)) { 
-        res.header('Access-Control-Allow-Origin', '*');
+        res.header('Access-Control-Allow-Origin', origin); 
     }
     
     next();
-} 
+}
 
+module.exports = cors; 
