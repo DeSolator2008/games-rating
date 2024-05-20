1ac0ab3cd0de808c5d26e810057babe236d4f193
DeSolator2008 <149343392+DeSolator2008@users.noreply.github.com>
2024-05-21 06:37:23 +1000

    Ver 2

--- app/api/config.js
+++ app/api/config.js
@@ -1,4 +1,4 @@
-export const BASE_URL = 'http://localhost:3001/api';
+export const BASE_URL = 'https://training-frontend-domen.nomoredomainswork.ru/api' 
 export const endpoints = {
   games: `${BASE_URL}/games`,
   auth: `${BASE_URL}/auth/local`,
