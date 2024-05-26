38918f1f8ed30ab75ecf61be4622bfdeacd73998
DeSolator2008 <149343392+DeSolator2008@users.noreply.github.com>
2024-05-27 07:33:09 +1000

    Ver 4.8

--- app/api/config.js
+++ app/api/config.js
@@ -1,4 +1,4 @@
-export const BASE_URL = "https://training-backend-domen.nomoredomainswork.ru";
+export const BASE_URL = "https://training-backend-domen.nomoredomainswork.ru/api";
 
 export const endpoints = {
   games: `${BASE_URL}/games`,
